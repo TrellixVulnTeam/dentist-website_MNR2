0c380b5005cfd73f0bc217d9edd8addb6fb24bac
VladislavPergler <pergler.vladislav@gmail.com>
2021-12-20 17:34:20 +0100

    js scroll

--- js/main.js
+++ js/main.js
@@ -42,3 +42,24 @@ const homeObserver = new IntersectionObserver(stickyNav, {
 });
 
 homeObserver.observe(home);
+
+// SECTION ANIMATION ON SCROLL
+const allSections = document.querySelectorAll(".section");
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove("section__hidden");
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add("section__hidden");
+});
